91acf1e3bd3d9c74b49b5da10a7643d3cb35718e
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-30 15:06:34 +0700

    Update user.js

--- cms/controller/manage/user.js
+++ cms/controller/manage/user.js
@@ -6,26 +6,22 @@ const {User} = require('../../models/user')
 UserManage.post('/getAll', async (req, res) => {
     const data = await User.find({},{_id:0,__v:0})
     res.status(200).json(data)
-
 })
 
 UserManage.post('/addUser', async (req, res) => {
     const newUser = new User(req.body)
     await newUser.save()
     res.status(200).json(newUser)
-
 })
 
 UserManage.post('/changePassword', async (req, res) => {
     const update = await User.updateOne({id: req.body.id}, {$set: {passWord: req.body.newPassWord}})
     res.status(200).json(update)
-
 })
 
 UserManage.put('/updateUser', async (req, res) => {
     const update = await User.updateOne({id: req.body.id}, {$set: req.body})
     res.status(200).json(update)
-
 })
 
 module.exports = UserManage
