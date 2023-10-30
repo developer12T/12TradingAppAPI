323f0730be1ed57537b63449568c410cda608ab5
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-30 17:17:55 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,11 +4,10 @@ const app = express()
 const cors = require('cors')
 app.use(express.json())
 
-  app.use(cors())
+app.use(cors())
 
-//manageUser
 const cms = require('./cms/index')
 
-app.use('/cms',cms)
+app.use('/cms', cms)
 
 module.exports = app
