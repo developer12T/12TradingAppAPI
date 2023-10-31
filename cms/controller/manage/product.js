d58692c38e649868a220b6318befacfa67640bca
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-31 10:00:24 +0700

    Update product.js

--- cms/controller/manage/product.js
+++ cms/controller/manage/product.js
@@ -8,4 +8,9 @@ ProductManage.post('/getAll', async (req, res) => {
     res.status(200).json(data)
 })
 
+ProductManage.post('/addProduct', async (req, res) => {
+    const data = await Product.find({},{_id:0,__v:0})
+    res.status(200).json(data)
+})
+
 module.exports = ProductManage
