c246cfae4478d39ee463745ca06ef0d8788158a9
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-31 14:07:34 +0700

    Update getProduct.js

--- cms/controller/saleProduct/getProduct.js
+++ cms/controller/saleProduct/getProduct.js
@@ -5,7 +5,7 @@ const { Product } = require('../../models/product')
 
 getProduct.post('/getAll', async (req, res) => {
     try {
-        const data = await Product.find()
+        const data = await Product.find({},{_id:0,id:1,name:1,skuList:1})
         res.status(200).json(data)
     } catch (e) {
         res.status(500).json(e)
