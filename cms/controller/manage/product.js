0b362eef23fa56bbd7b50572b957227efa8e9037
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-31 13:58:11 +0700

    .

--- cms/controller/manage/product.js
+++ cms/controller/manage/product.js
@@ -2,29 +2,32 @@ const express = require('express')
 require('../../configs/connect')
 const ProductManage = express.Router()
 const {Product} = require('../../models/product')
-const {Route} = require("../../models/route");
 
 ProductManage.post('/getAll', async (req, res) => {
-    const data = await Product.find({},{_id:0,__v:0})
+    const data = await Product.find({}, {_id: 0, __v: 0})
     res.status(200).json(data)
 })
 
 ProductManage.post('/addProduct', async (req, res) => {
-    const idInsert = await Product.findOne({},{_id:0,idIndex:1}).sort({idIndex:-1})
-    if(idInsert === null ){
-        var idIndex = 1
-    }else{
-        var idIndex = idInsert.idIndex + 1
+    try {
+        const idInsert = await Product.findOne({}, {_id: 0, idIndex: 1}).sort({idIndex: -1})
+        if (idInsert === null) {
+            var idIndex = 1
+        } else {
+            var idIndex = idInsert.idIndex + 1
+        }
+        req.body.idIndex = idIndex
+        const newRoute = new Product(req.body)
+        await newRoute.save()
+        res.status(200).json(newRoute)
+    } catch (e) {
+        res.status(500).json(e)
     }
-    req.body.idIndex = idIndex
-    const newRoute = new Product(req.body)
-    await newRoute.save()
-    res.status(200).json(newRoute)
 })
 
 ProductManage.put('/updateProduct', async (req, res) => {
-    const data = await Product.find({},{_id:0,__v:0})
-    res.status(200).json('update product')
+    const data = await Product.updateOne({id: req.body.id}, {$set: [req.body]})
+    res.status(200).json(data)
 })
 
 module.exports = ProductManage
