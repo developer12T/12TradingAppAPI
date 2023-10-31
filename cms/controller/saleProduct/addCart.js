838cdb0faf55a9169803a5feeb8b66d517154576
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-10-31 16:09:25 +0700

    .

--- cms/controller/saleProduct/addCart.js
+++ cms/controller/saleProduct/addCart.js
@@ -1,13 +1,39 @@
 const express = require('express')
 
 require('../../configs/connect')
+const {Cart} = require("../../models/saleProduct")
+const {Checkin} = require("../../models/route");
 const addCart = express.Router()
 
 addCart.post('/addCart', async (req, res) => {
     try {
+        var totalPrice_bath = 0
+        const priceData = await Cart.findOne({area: req.body.area, storeId: req.body.storeId}, {totalPrice: 1})
+        if (priceData === null) {
+            for (let i = 0; i < req.body.list.length; i++) {
+                console.log(req.body.list[i].id)
+                totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
+            }
+            console.log(totalPrice_bath)
+            req.body.totalPrice = totalPrice_bath
+            const addCart = new Cart(req.body)
+            await addCart.save()
+            res.status(200).json('addCart')
+        } else {
 
-        res.status(200).json('add cart --')
+            for (let i = 0; i < req.body.list.length; i++) {
+                console.log(req.body.list[i].id)
+                totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
+                await Cart.updateOne({
+                    area: req.body.area,
+                    storeId: req.body.storeId
+                }, {$push: {list: req.body.list[i]}})
 
+            }
+            var summaryPrice = totalPrice_bath + priceData.totalPrice
+            await Cart.updateOne({area: req.body.area, storeId: req.body.storeId}, {$set: {totalPrice: summaryPrice}})
+            res.status(200).json(summaryPrice)
+        }
     } catch (e) {
         res.status(500).json(e)
     }
--- cms/controller/saleProduct/getProduct.js
+++ cms/controller/saleProduct/getProduct.js
@@ -1,11 +1,11 @@
 const express = require('express')
 require('../../configs/connect')
 const getProduct = express.Router()
-const { Product } = require('../../models/product')
+const {Product} = require('../../models/product')
 
 getProduct.post('/getAll', async (req, res) => {
     try {
-        const data = await Product.find({},{_id:0,id:1,name:1,skuList:1})
+        const data = await Product.find({}, {_id: 0, id: 1, name: 1, skuList: 1})
         res.status(200).json(data)
     } catch (e) {
         res.status(500).json(e)
