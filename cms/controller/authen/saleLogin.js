b4733656572f2d342b201400c48a551dbe0a8fab
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-11-03 10:55:55 +0700

    .

--- cms/controller/authen/saleLogin.js
+++ cms/controller/authen/saleLogin.js
@@ -3,12 +3,15 @@ const express = require('express')
 require('../../configs/connect')
 const saleLogin = express.Router()
 const {Route, Checkin} = require('../../models/route')
+const {User} = require("../../models/user");
 
 saleLogin.post('/login', async (req, res) => {
     try {
-        res.status(200).json('test login --')
+        const data = await User.findOne()
+        res.status(200).json(data)
+
     } catch (e) {
-        res.status(500).json(e)
+        res.status(500).json(e.message)
     }
 })
 
