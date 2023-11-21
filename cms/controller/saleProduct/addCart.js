535cbc7e5ab2d304f83481e575de3a0e5944ddc3
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-11-21 11:27:06 +0700

    .

--- cms/controller/saleProduct/addCart.js
+++ cms/controller/saleProduct/addCart.js
@@ -5,36 +5,80 @@ const {Cart} = require("../../models/saleProduct")
 const {Checkin} = require("../../models/route");
 const addCart = express.Router()
 
-addCart.post('/addCart', async (req, res) => {
+// addCart.post('/addCart', async (req, res) => {
+//     try {
+//         var totalPrice_bath = 0
+//         const priceData = await Cart.findOne({area: req.body.area, storeId: req.body.storeId}, {totalPrice: 1})
+//         if (priceData === null) {
+//             for (let i = 0; i < req.body.list.length; i++) {
+//                 console.log(req.body.list[i].id)
+//                 totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
+//             }
+//             console.log(totalPrice_bath)
+//             req.body.totalPrice = totalPrice_bath
+//             const addCart = new Cart(req.body)
+//             await addCart.save()
+//             res.status(200).json('addCart')
+//         } else {
+//
+//             for (let i = 0; i < req.body.list.length; i++) {
+//                 console.log(req.body.list[i].id)
+//                 totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
+//                 await Cart.updateOne({
+//                     area: req.body.area,
+//                     storeId: req.body.storeId
+//                 }, {$push: {list: req.body.list[i]}})
+//             }
+//             var summaryPrice = totalPrice_bath + priceData.totalPrice
+//             await Cart.updateOne({area: req.body.area, storeId: req.body.storeId}, {$set: {totalPrice: summaryPrice}})
+//             res.status(200).json('update Successfully')
+//         }
+//     } catch (e) {
+//         res.status(500).json({
+//             status: 500,
+//             message: e.message
+//         })
+//     }
+// })
+
+addCart.post('/addProductToCart', async (req, res) => {
     try {
         var totalPrice_bath = 0
         const priceData = await Cart.findOne({area: req.body.area, storeId: req.body.storeId}, {totalPrice: 1})
         if (priceData === null) {
-            for (let i = 0; i < req.body.list.length; i++) {
-                console.log(req.body.list[i].id)
-                totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
-            }
-            console.log(totalPrice_bath)
+
+            // console.log(req.body.list.id)
+            totalPrice_bath = totalPrice_bath + (req.body.list.qty * req.body.list.pricePerQty)
+
+            // console.log(totalPrice_bath)
             req.body.totalPrice = totalPrice_bath
             const addCart = new Cart(req.body)
             await addCart.save()
-            res.status(200).json('addCart')
+            res.status(200).json({
+                status: 200,
+                message: 'addProduct to cart Successfully'
+            })
         } else {
 
-            for (let i = 0; i < req.body.list.length; i++) {
-                console.log(req.body.list[i].id)
-                totalPrice_bath = totalPrice_bath + (req.body.list[i].qty * req.body.list[i].pricePerQty)
-                await Cart.updateOne({
-                    area: req.body.area,
-                    storeId: req.body.storeId
-                }, {$push: {list: req.body.list[i]}})
-            }
+            console.log(req.body.list.id)
+            totalPrice_bath = totalPrice_bath + (req.body.list.qty * req.body.list.pricePerQty)
+            await Cart.updateOne({
+                area: req.body.area,
+                storeId: req.body.storeId
+            }, {$push: {list: req.body.list}})
+
             var summaryPrice = totalPrice_bath + priceData.totalPrice
             await Cart.updateOne({area: req.body.area, storeId: req.body.storeId}, {$set: {totalPrice: summaryPrice}})
-            res.status(200).json('update Successfully')
+            res.status(200).json({
+                status: 200,
+                message: 'update Successfully'
+            })
         }
     } catch (e) {
-        res.status(500).json(e.message)
+        res.status(500).json({
+            status: 500,
+            message: e.message
+        })
     }
 })
 
--- cms/controller/saleProduct/getProduct.js
+++ cms/controller/saleProduct/getProduct.js
@@ -2,7 +2,7 @@ const express = require('express')
 require('../../configs/connect')
 const getProduct = express.Router()
 const {Product} = require('../../models/product')
-const  axios  = require('axios')
+const axios = require('axios')
 
 getProduct.post('/getAll', async (req, res) => {
     try {
@@ -15,11 +15,70 @@ getProduct.post('/getAll', async (req, res) => {
 
 getProduct.post('/getDetail', async (req, res) => {
     try {
-        const data = await Product.findOne({id:req.body.id}, {_id: 0, id: 1, name: 1, skuList: 1})
+        const data = await Product.findOne({id: req.body.id}, {_id: 0, id: 1, name: 1, skuList: 1})
         res.status(200).json(data)
     } catch (error) {
         res.status(500).json(error.message)
     }
+
 })
+
+getProduct.post('/getDetailUnit', async (req, res) => {
+    try {
+        const data = await Product.findOne({id: req.body.id}, {_id: 0, id: 1, name: 1, skuList: 1})
+        // const sumPrice = 0
+        var priceUnit = 0
+
+        for(const list of data.skuList){
+            if(list.id === req.body.unitId){
+                priceUnit = list.pricePerSku
+            }
+        }
+
+        const mainData = {
+            id:data.id,
+            name:data.name,
+            unitId:req.body.unitId,
+            qty:req.body.qty,
+            sumPrice:priceUnit*req.body.qty,
+            skuList:data.skuList
+        }
+        res.status(200).json(mainData)
+    } catch (error) {
+        res.status(500).json({
+            status: 500,
+            message: error.message
+        })
+    }
+})
+
+// getProduct.post('/getDetailQty', async (req, res) => {
+//     try {
+//         const data = await Product.findOne({id: req.body.id}, {_id: 0, id: 1, name: 1, skuList: 1})
+//         // const sumPrice = 0
+//         var priceUnit = 0
+//
+//         for(const list of data.skuList){
+//             if(list.id === req.body.unitId){
+//                 priceUnit = list.pricePerSku
+//             }
+//         }
+//
+//         const mainData = {
+//             id:data.id,
+//             name:data.name,
+//             unitId:req.body.unitId,
+//             qty:req.body.qty,
+//             sumPrice:priceUnit*req.body.qty,
+//             skuList:data.skuList
+//         }
+//         res.status(200).json(mainData)
+//     } catch (error) {
+//         res.status(500).json({
+//             status: 500,
+//             message: error.message
+//         })
+//     }
+// })
 
 module.exports = getProduct
