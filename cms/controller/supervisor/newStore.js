5f2c3006e26bc31343a825a17ea90e2b2fb4f0ac
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-11-22 16:51:08 +0700

    Update newStore.js

--- cms/controller/supervisor/newStore.js
+++ cms/controller/supervisor/newStore.js
@@ -1,29 +1,35 @@
 const express = require('express')
 require('../../configs/connect')
 const newStore = express.Router()
-const { Store } = require('../../models/store')
+const {Store} = require('../../models/store')
 
-newStore.put('/newStore', async(req, res) => {
-    const { currentdateDash } = require('../../utils/utility.js')
+newStore.put('/newStore', async (req, res) => {
+    const {currentdateDash} = require('../../utils/utility.js')
     try {
-        if(req.body.id !== undefined && req.body.id !== ''){
-            if(req.body.appPerson !== undefined && req.body.appPerson !== ''){
-                if(req.body.status !== undefined && req.body.status !== ''){
-                    await Store.updateOne({idNumber:req.body.id}, { $set: { 'approve.status': req.body.status,'approve.dateAction':currentdateDash(),'approve.appPerson':req.body.appPerson,status:'1'} })
-                    res.status(200).json({message:'Store approve/not approve complete!'})
-                }else{
-                    res.status(501).json({message:'require data!'})      
-                } 
-            }else{
-                res.status(501).json({message:'require data!'})      
+        if (req.body.id !== undefined && req.body.id !== '') {
+            if (req.body.appPerson !== undefined && req.body.appPerson !== '') {
+                if (req.body.status !== undefined && req.body.status !== '') {
+                    await Store.updateOne({idNumber: req.body.id}, {
+                        $set: {
+                            'approve.status': req.body.status,
+                            'approve.dateAction': currentdateDash(),
+                            'approve.appPerson': req.body.appPerson,
+                            status: '1'
+                        }
+                    })
+                    res.status(200).json({status: 200, message: 'Store approve/not approve complete!'})
+                } else {
+                    res.status(501).json({status: 501, message: 'require data!'})
+                }
+            } else {
+                res.status(501).json({status: 501, message: 'require data!'})
             }
-        }else{
-            res.status(501).json({message:'require data!'})       
+        } else {
+            res.status(501).json({status: 501, message: 'require data!'})
         }
     } catch (error) {
-        console.log(error)
-        res.status(500).json(error.message)
-    } 
+        res.status(500).json({status: 500, message: error.message})
+    }
 })
 
 module.exports = newStore
