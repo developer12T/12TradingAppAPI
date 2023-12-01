6601bf7b368c8c91b2e14ac04eb68310a913a792
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2023-12-01 16:34:35 +0700

    Update getProduct.js

--- cms/controller/refund/getProduct.js
+++ cms/controller/refund/getProduct.js
@@ -34,15 +34,31 @@ getProduct.post('/getProductDetail', async (req, res) => {
                 res.status(200).json(mainData)
                 break
             case 'change':
+                const dataChange = await Product.findOne({id: req.body.id,'unitList.id':'1'}, {_id: 0, id: 1, name: 1, unitList:1})
+                for(const list of  dataChange.unitList){
+                    if(list.id === req.body.unitId){
+                        var priceUnit = list.pricePerUnitChange
+                    }
+                }
+                // console.log(priceUnit)
+                const mainDataChange = {
+                    id:dataChange.id,
+                    name:dataChange.name,
+                    unitId:req.body.unitId,
+                    priceUnit:priceUnit,
+                    qty:req.body.qty,
+                    sumPrice: req.body.qty * priceUnit,
+                    list:dataChange.unitList
+                }
+                res.status(404).json(mainDataChange)
                 break
             default:
-                res.status(404).json({status:404,message:'Server Not Found'})
+                res.status(404).json({status:404,message:'Server Response Not Found'})
                 break
         }
     } catch (error) {
         res.status(500).json({status:500,message:error.message})
     }
-
 })
 
 getProduct.post('/getProductDetailUnit', async (req, res) => {
@@ -68,11 +84,32 @@ getProduct.post('/getProductDetailUnit', async (req, res) => {
                     unitList:data.unitList
                 }
                 res.status(200).json(mainData)
-
                 break
-            case 'change':
+
+            case 'change' :
+                const dataChange = await Product.findOne({id: req.body.id,'unitList.id':'1'}, {_id: 0, id: 1, name: 1, unitList: 1})
+                // const sumPrice = 0
+                var priceUnit = 0
+
+                for(const list of dataChange.unitList){
+                    if(list.id === req.body.unitId){
+                        priceUnit = list.pricePerUnitRefund
+                    }
+                }
+
+                const mainDataChange = {
+                    id:dataChange.id,
+                    name:dataChange.name,
+                    unitId:req.body.unitId,
+                    qty:req.body.qty,
+                    sumPrice:priceUnit*req.body.qty,
+                    unitList:dataChange.unitList
+                }
+
+                res.status(200).json(mainDataChange)
                 break
             default:
+                res.status(404).json({status:404,message:'Server Response Not Found'})
                 break
         }
     } catch (error) {
