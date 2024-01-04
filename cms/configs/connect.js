0e0568c5e014ef7814ef3f3f1ecf650175d01319
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2024-01-04 09:42:43 +0700

    Update connect.js

--- cms/configs/connect.js
+++ cms/configs/connect.js
@@ -1,20 +1,35 @@
-const mongoose = require('mongoose') 
-const dotenv = require('dotenv')
-dotenv.config() 
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const { CONNECT_STRING } = process.env
+const { CONNECT_STRING } = process.env;
 
 const connectDB = async () => {
     try {
-      await mongoose.connect(CONNECT_STRING, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-      });
-      console.log('\x1b[35mConnected to MongoDB Success\x1b[0m')
-    } catch (error) {
-      console.error('Error connecting to MongoDB:', error)
-      throw error
-    }
-  };
+        let progress = 0;
+        const totalSteps = 10; // จำนวนขั้นตอนทั้งหมดที่คาดว่าจะใช้
 
-module.exports = connectDB
+         await mongoose.connect(CONNECT_STRING, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+
+        const interval = await setInterval(() => {
+            progress += 1;
+            const percentage = (progress / totalSteps) * 100;
+            process.stdout.clearLine();
+            process.stdout.cursorTo(0);
+            process.stdout.write(`\x1b[35mConnecting to MongoDB... ${percentage.toFixed(2)}%\x1b[0m`);
+
+            if (progress === totalSteps) {
+                clearInterval(interval);
+                console.log('\n\x1b[35mConnected to MongoDB Success\x1b[0m');
+            }
+        }, 200); // ปรับความถี่ของการอัพเดทตามต้องการ
+    } catch (error) {
+        console.error('Error connecting to MongoDB:', error);
+        throw error;
+    }
+};
+
+module.exports = connectDB;
