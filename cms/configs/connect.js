0a5b50988c15f774cf6845d127d5cb2350b674e6
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2024-01-04 09:27:57 +0700

    Update connect.js

--- cms/configs/connect.js
+++ cms/configs/connect.js
@@ -10,7 +10,7 @@ const connectDB = async () => {
         useNewUrlParser: true,
         useUnifiedTopology: true
       });
-      console.log('Connected to MongoDB Success')
+      console.log('\x1b[35mConnected to MongoDB Success\x1b[0m')
     } catch (error) {
       console.error('Error connecting to MongoDB:', error)
       throw error
