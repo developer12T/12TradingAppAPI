f4ac3f98b6a9ad31e9de76e9f375455bc6fec71c
woraphat12T <137477332+woraphat12T@users.noreply.github.com>
2024-01-08 14:17:01 +0700

    .

--- cms/controller/promotion/getPromotion.js
+++ cms/controller/promotion/getPromotion.js
@@ -1,4 +1,3 @@
-
 const express = require('express')
 require('../../configs/connect')
 const {Promotion} = require("../../models/promotion");
@@ -19,28 +18,31 @@ getPromotion.post('/getPromotion', async (req, res) => {
 
 getPromotion.post('/getDetail', async (req, res) => {
     try {
-        const data = await Promotion.findOne({proId:req.body.proId},{itemfree:1,proId:1,_id:0})
+        const data = await Promotion.findOne({proId: req.body.proId}, {itemfree: 1, proId: 1, _id: 0})
         var dataItem = []
-        for(const list of data.itemfree){
-            if(!list.productId){
+        for (const list of data.itemfree) {
+            if (!list.productId) {
                 console.log('เป็นแถมแบบ group')
                 const dataRewardItem = await Product.find({
                     group: list.productGroup,
                     size: list.productSize,
-                    "convertFact.unitId": { $ne: '3' }
-                },{id:1,_id:0,name:1})
+                    "convertFact.unitId": {$ne: '3'}
+                }, {id: 1, _id: 0, name: 1})
 
-                for(const sublist of dataRewardItem){
+                for (const sublist of dataRewardItem) {
                     dataItem.push(sublist)
                 }
-            }else {
-                console.log('เป็นแถมระบุ ไอเท็ม')
+            } else {
+                console.log('เป็นแถมระบุ ไอเท็ม ::' + list.productId)
+                const dataRewardItem = await Product.findOne({id: list.productId}, {_id: 0,id: 1,name: 1})
+                dataItem.push(dataRewardItem)
                 // promotion product
             }
         }
+
         const mainData = {
-            promotionData:data.itemfree,
-            itemFree:dataItem
+            promotionData: data.itemfree,
+            itemFree: dataItem
         }
         res.status(200).json(mainData)
     } catch (e) {
