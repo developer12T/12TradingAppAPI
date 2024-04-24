deefd4d92b95aa3f5938c6e002f4c3ff32fc1897
aphinet12T <aphinet.dam@onetwotrading.co.th>
2024-04-24 17:28:43 +0700

    .

--- app.js
+++ app.js
@@ -6,16 +6,16 @@ app.use(express.json())
 
 app.use(cors())
 
-app.use('/cms', (req, res, next) => {
-    console.log(req.path)
-    if (req.path === '/authen/login') { // ยกเว้น api ให้ใช้ next()
-         // console.log(req.path)
-        next()
-    } else {
-        // ตรวจสอบ Token สำหรับเส้นทางอื่น ๆ ใน /cms
-        auth(req, res, next)
-    }
-}, express.static('public'))
+// app.use('/cms', (req, res, next) => {
+//     console.log(req.path)
+//     if (req.path === '/authen/login') { // ยกเว้น api ให้ใช้ next()
+//          // console.log(req.path)
+//         next()
+//     } else {
+//         // ตรวจสอบ Token สำหรับเส้นทางอื่น ๆ ใน /cms
+//         auth(req, res, next)
+//     }
+// }, express.static('public'))
 
 // const verifyToken = require('./cms/controller/authen/middleware/authen');
 const cms = require('./cms/index')
