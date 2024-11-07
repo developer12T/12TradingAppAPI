7d337831c5c7bfb83cd9fe80745a220eebee1a93
aphinet12T <aphinet.dam@onetwotrading.co.th>
2024-11-07 21:26:07 +0700

    Update addCnOrder.js

--- cms/controller/cnOrder/addCnOrder.js
+++ cms/controller/cnOrder/addCnOrder.js
@@ -79,7 +79,7 @@ addCnOrder.post('/addCnOrder', async (req, res) => {
                 dateShip: null,
                 note: null
             },
-            status: '0',
+            status: '10',
             createDate: currentdateSlash(),
             updateDate: null,
             refOrder: refOrder
